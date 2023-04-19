bd629ff44d3908fb792c5e47c313efd9b7378b43
Sambit Sekhar Pattanaik <Sambit.Pattanaik@nielseniq.com>
2023-04-19 15:43:30 +0530

    first upload

---
+++ AndroidVoice/MainActivity.cs
@@ -0,0 +1,139 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidVoiceLibrary;
+using System;
+using AndroidX.AppCompat.Widget;
+using Android.Content;
+
+namespace AndroidVoice
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private ImageView recButton;
+        private VoiceService voiceService;
+        private string languageCode;
+        private int SPEECH_REQUEST_CODE = 0;
+        private TextView TView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            languageCode = null;
+            TView = FindViewById<TextView>(Resource.Id.textView1);
+
+            //-------------------||If we want to set the text in search view||-------------------
+            //searchView = FindViewById<SearchView>(Resource.Id.searchView1);
+            //searchView.SetOnQueryTextListener(this);
+
+            //-------------------||If you want to support multiple language||------------------
+
+            //// Get the spinner widget
+            //Spinner spinner = FindViewById<Spinner>(Resource.Id.spinnerLanguage);
+
+            //// Set up the spinner adapter
+            //ArrayAdapter adapter1 = ArrayAdapter.CreateFromResource(this, Resource.Array.languages, Android.Resource.Layout.SimpleSpinnerItem);
+            //adapter1.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            //spinner.Adapter = adapter1;
+
+            //string defaultLanguageCode = Java.Util.Locale.Default.Language;
+
+
+
+            //// Set the initial language to Default
+            //int initialLanguageIndex = 0;
+            //spinner.SetSelection(initialLanguageIndex);
+
+            //// Set up the spinner item selected listener
+            //spinner.ItemSelected += (sender, e) =>
+            //{
+            //    languageCode = null;
+
+            //    switch (e.Position)
+            //    {
+            //        case 0:
+            //            // Default Android language
+            //            languageCode = defaultLanguageCode;
+            //            break;
+            //        case 1:
+            //            // Chinese(PRC) language
+            //            languageCode = "zh-CN";
+            //            break;
+            //        case 2:
+            //             //Japanese language
+            //            languageCode = "ja-JP";
+            //            break;
+
+
+            //---------Give as many language code as you need
+    //------------------------------------------------------------------------------------------------
+            //------also add the languages in the strings.xml of your app like given below
+            //<string-array name="languages">
+
+            //<item>Default</item>
+            //<item>Chinese(PRC)</item>
+            //<item>Japanese</item>
+
+            //as many language as you want to add
+
+            //</string-array>
+    //---------------------------------------------------------------------------------------------
+            //        default:
+            //            // Default Android language
+            //            languageCode = defaultLanguageCode;
+            //            break;
+
+            //    }
+
+            //    // Update SPEECH_REQUEST_CODE variable to keep track of the current language
+            //    SPEECH_REQUEST_CODE = e.Position;
+
+            //};
+
+            string defaultLanguageCode = Java.Util.Locale.Default.Language;
+            languageCode = defaultLanguageCode;
+
+            recButton = FindViewById<ImageView>(Resource.Id.btnRecord);
+
+            voiceService = new VoiceService();
+
+            recButton.Click += delegate
+            {
+                voiceService.StartVoiceRecognition(this, OnVoiceRecognitionResult, languageCode, SPEECH_REQUEST_CODE);
+            };
+        }
+
+        //-------------------------||If you want to set the text in the search view||---------------------
+        //private void OnVoiceRecognitionResult(string result)
+        //{
+        //    // Do something with the voice recognition result
+        //    searchView.SetQuery(result, true);
+        //}
+
+    private void OnVoiceRecognitionResult(string result)
+        {
+            // Do something with the voice recognition result
+            TView.SetText(result, TextView.BufferType.Normal);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            // Call the OnResults method of the voiceService instance
+            voiceService.OnResults(requestCode, resultCode, data, SPEECH_REQUEST_CODE);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ VoiceLib/VoiceService.cs
@@ -0,0 +1,150 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Speech;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroidVoiceLibrary
+{
+    public class VoiceService : Java.Lang.Object, IRecognitionListener
+    { 
+        private SpeechRecognizer speechRecognizer;
+        private Action<string> callback;
+        public void StartVoiceRecognition(Activity activity, Action<string> callback, string languageCode, int SPEECH_REQUEST_CODE)
+        {
+            this.callback = callback;
+
+            // Initialize speech recognizer
+            speechRecognizer = SpeechRecognizer.CreateSpeechRecognizer(activity);
+            speechRecognizer.SetRecognitionListener(this);
+
+
+            // Check for microphone
+            string rec = Android.Content.PM.PackageManager.FeatureMicrophone;
+            if (rec != "android.hardware.microphone")
+            {
+                var alert = new Android.App.AlertDialog.Builder(activity);
+                alert.SetTitle("You don't seem to have a microphone to record with");
+                alert.SetPositiveButton("OK", (sender, e) =>
+                {
+                    return;
+                });
+                alert.Show();
+                return;
+            }
+
+            // Create speech recognition intent
+            var voiceIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraPrompt, "speak now!");
+            voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 5000);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 5000);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 30000);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
+            voiceIntent.PutExtra(RecognizerIntent.ExtraLanguage, languageCode);
+
+            // Start speech recognition activity
+            activity.StartActivityForResult(voiceIntent, SPEECH_REQUEST_CODE);
+        }
+
+        public void OnResults(int requestCode, Result resultCode, Intent data, int SPEECH_REQUEST_CODE)
+        {
+
+
+            if (requestCode == SPEECH_REQUEST_CODE && resultCode == Result.Ok)
+            {
+                // Get speech recognition results
+                var matches = data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+
+                if (matches != null && matches.Count > 0)
+                {
+                    // Display first result in text view
+                    var text = matches[0];
+                    callback.Invoke(text);
+                }
+            }
+        }
+
+        public void OnBeginningOfSpeech()
+        {
+            // Not used
+        }
+
+        public void OnBufferReceived(byte[] buffer)
+        {
+            // Not used
+        }
+
+        public void OnEndOfSpeech()
+        {
+            // Not used
+        }
+        public void OnError(SpeechRecognizerError error)
+        {
+            try
+            {
+                if (error == SpeechRecognizerError.NoMatch)
+                {
+                    // handle "could not catch, try again" error
+                    speechRecognizer.Cancel();
+                    speechRecognizer.StartListening(new Intent(RecognizerIntent.ActionRecognizeSpeech));
+                }
+                else if (error == SpeechRecognizerError.SpeechTimeout)
+                {
+                    // handle speech timeout error
+                    callback.Invoke("Speech recognition timeout");
+                }
+                else if (error == SpeechRecognizerError.NetworkTimeout)
+                {
+                    // handle network timeout error
+                    callback.Invoke("Network timeout");
+                }
+                else
+                {
+                    // handle other errors
+                    callback.Invoke($"Error: {error.ToString()}");
+                }
+            }
+           
+            catch (Java.Lang.Exception e)
+            {
+                // handle exception
+                callback.Invoke($"Error: {e.Message}");
+            }
+           
+        }
+        public void OnEvent(int eventType, Bundle @params)
+        {
+            // Not used
+        }
+
+        public void OnPartialResults(Bundle partialResults)
+        {
+            // Not used
+        }
+
+        public void OnReadyForSpeech(Bundle @params)
+        {
+            // Not used
+        }
+
+        public void OnRmsChanged(float rmsdB)
+        {
+            // Not used
+        }
+
+        public void OnResults(Bundle results)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
